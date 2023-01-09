6ed298647ebf021a06822057056ba0e75c212f51
Haris Mohanty <108195360+Haris-Mohanty@users.noreply.github.com>
2023-01-09 14:55:37 +0000

    Budget set in localstorage successfully.

---
+++ script.js
@@ -0,0 +1,15 @@
+//START SET BUDGET CODE
+let setBudget = document.getElementById("set-budget");
+let budgetBtn = document.getElementById("budget-btn");
+let totalBudget = document.getElementById("total-budget");
+budgetBtn.addEventListener("click", () => {
+  setBudgetFun();
+});
+const setBudgetFun = () => {
+  if(setBudget.value != ""){
+    localStorage.setItem("button",setBudget.value);
+  }else{
+    swal("Input Field Empty!", "Please Enter a Budget !", "warning");
+  }
+};
+//END SET BUDGET CODE
