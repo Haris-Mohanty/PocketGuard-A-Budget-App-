a8dfd89f7bbc3354d981862a21862468ea3b0d93
Haris Mohanty <108195360+Haris-Mohanty@users.noreply.github.com>
2023-02-08 04:31:58 +0000

    Project Finished.

--- script.js
+++ script.js
@@ -107,8 +107,7 @@ let del_btn = document.getElementsByClassName("del-btn");
       title.focus();
       product_btn.innerHTML = "Click to Update";
 
-      
-      product_btn.onclick = function(){
+    product_btn.onclick = function(){
         localStorage.removeItem("budget_"+product_edit);
         let p_title = title.value;
         let p_cost = cost.value;
@@ -118,7 +117,7 @@ let del_btn = document.getElementsByClassName("del-btn");
         let string = JSON.stringify(data);
         localStorage.setItem("budget_"+title.value, string);
         location.href = location.href;
-      }
+    }
     }
   }
   //END EDIT BUTTON CODE
